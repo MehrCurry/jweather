9cb4a420903f5e89097c48af4159d44c45ef89e5
Guido Zockoll <gzockoll@gmail.com>
2012-02-16 06:36:51 +0100

    changed to latest commons-httpclient added osgi plugin

--- src/main/java/net/sf/jweather/metar/MetarFetcher.java
+++ src/main/java/net/sf/jweather/metar/MetarFetcher.java
@@ -17,27 +17,27 @@ License along with this library; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 For more information, please email arimus@users.sourceforge.net
-*/
+ */
 package net.sf.jweather.metar;
 
-import java.io.*;
-import java.net.*;
-import org.apache.log4j.Logger;
+import java.io.IOException;
+
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpMethod;
-import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.HttpRecoverableException;
+import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.log4j.Logger;
 
 /**
- * Responsible for downloading the METAR reports 
- *
+ * Responsible for downloading the METAR reports
+ * 
  * <p>
  * <code>
  * String metarData = MetarFetcher.fetch("KLAX");<br>
  * </code>
  * </p>
- *
+ * 
  * @author David Castro, dcastro@apu.edu
  * @version $Revision: 1.4 $
  * @see <a href="Metar.html">Metar</a>
@@ -47,12 +47,13 @@ public class MetarFetcher {
 	private static String metarData = null;
 
 	final static String httpMetarURL = "http://weather.noaa.gov/pub/data/observations/metar/stations/";
-	//final static String httpMetarHostname =  "weather.noaa.gov";
-	//final static int    httpMetarPort     =  80;
-	//final static String httpMetarPath     = "/pub/data/observations/metar/stations/";
+	// final static String httpMetarHostname = "weather.noaa.gov";
+	// final static int httpMetarPort = 80;
+	// final static String httpMetarPath =
+	// "/pub/data/observations/metar/stations/";
 
 	static {
-    	log = Logger.getLogger("net.sf.jweather");
+		log = Logger.getLogger("net.sf.jweather");
 		log.debug("MetarFetcher: instantiated");
 	}
 
@@ -68,12 +69,13 @@ public class MetarFetcher {
 
 		// set the timeout is specified
 		if (timeout != 0) {
-			log.debug("MetarFetch: setting timeout to '"+timeout+"' milliseconds");
+			log.debug("MetarFetch: setting timeout to '" + timeout
+					+ "' milliseconds");
 			long start = System.currentTimeMillis();
 			client.setConnectionTimeout(timeout);
 			long end = System.currentTimeMillis();
 			if (end - start < timeout) {
-				client.setTimeout((int)(end - start));
+				client.setTimeout((int) (end - start));
 			} else {
 				return null;
 			}
@@ -84,26 +86,32 @@ public class MetarFetcher {
 
 		// connect to the NOAA site, retrying up to the specified num
 		int statusCode = -1;
-		//for (int attempt = 0; statusCode == -1 && attempt < 3; attempt++) {
-			try {
-				// execute the get method
-				log.debug("MetarFetcher: downloading data for station '"+station+"'");
-				statusCode = client.executeMethod(method);
-			} catch (HttpRecoverableException e) {
-				log.error("a recoverable exception occurred, " +
-						  "retrying." + e.getMessage());
-			} catch (IOException e) {
-				log.error("failed to download file: "+e);
-			}
-		//}
+		// for (int attempt = 0; statusCode == -1 && attempt < 3; attempt++) {
+		try {
+			// execute the get method
+			log.debug("MetarFetcher: downloading data for station '" + station
+					+ "'");
+			statusCode = client.executeMethod(method);
+		} catch (HttpRecoverableException e) {
+			log.error("a recoverable exception occurred, " + "retrying."
+					+ e.getMessage());
+		} catch (IOException e) {
+			throw new RuntimeException("failed to download file", e);
+		}
+		// }
 
 		// check that we didn't run out of retries
 		if (statusCode != HttpStatus.SC_OK) {
-			log.error("failed to download station data for '"+station+"'");
+			log.error("failed to download station data for '" + station + "'");
 			return null;
 		} else {
 			// read the response body
-			byte[] responseBody = method.getResponseBody();
+			byte[] responseBody;
+			try {
+				responseBody = method.getResponseBody();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
 
 			// release the connection
 			method.releaseConnection();
